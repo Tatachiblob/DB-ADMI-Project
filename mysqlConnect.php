45117c6287a205424b4c3af0d46842c328737af7
Tatachiblob <inoueyuta53@gmail.com>
2017-08-05 14:36:33 +0800

    Make your own $conn

--- mysqlConnect.php
+++ mysqlConnect.php
@@ -3,7 +3,6 @@ $serverName = "localhost";
 $userName = "root";
 $password = "ASDkmdsdmkasdmal";
 $dbName = "movierentaldb";
-$conn = new mysqli($serverName, $userName, "", $dbName);
 
 if($conn->connect_error){
   die("Connection Failed: " . $conn->connect_error);
